0155262e4defb88d64de4a3e4f99f72dc6a0b65f
Anthony Navarre <resonant.freakuency@gmail.com>
2019-03-28 08:57:37 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  [
+    { first_name: 'Bob', last_name: 'Ross' },
+    { first_name: 'Grete', last_name: 'Hermann' },
+    { first_name: 'Narendranath', last_name: 'Datta' }
+  ].each do |data|
+    Student.find_or_create_by!(data)
+  end
+
+  [
+    { name: '2020 Trip to the Denver Aquarium' }
+  ].each do |data|
+    FieldTrip.find_or_create_by!(data)
+  end
+end
